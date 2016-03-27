071fdf1164e3768f43c463aedf8c51f822e695f4
Alok Sharma <aloks1701@gmail.com>
2016-03-27 15:20:09 +0530

    Update BinarySearchRecursive.c
    Debugged the run time error in case the element was not in the array.

--- BinarySearchRecursive.c
+++ BinarySearchRecursive.c
@@ -22,15 +22,17 @@ void printArray(int *array, int size)
 
 int BinarySearchRecursive(int *array, int low, int high, int key)
 {
+    if(low> high)
+        return -1;      
+
     int middle = low +(high-low)/2; //since in case high + low is too high, it may cause overflow
     if(array[middle]==key)
-        return middle;
-    else if(array[middle]<key) //if key is bigger than element at middle index, then search in upper half
-        BinarySearchRecursive(array,middle+1,high,key);
-    else if(array[middle]>key) //else if key is smaller than element at middle index, then search in lower half
-        BinarySearchRecursive(array,low,middle-1,key);
-    else
-        return -1; //if element not found, return 1
+        {return middle;}
+    else if(array[middle]<key) //if key is bigger than search in upper half
+        {BinarySearchRecursive(array,middle+1,high,key);}
+    else if(array[middle]>key)
+        {BinarySearchRecursive(array,low,middle-1,key);}
+
 }
 
 
@@ -41,15 +43,17 @@ int main(void)
     scanf("%d",&high);
     int array[high];
     printf("Enter the elements in increasing order in the array of size %d  \n",high);
-    getArray(array, high); //Binary Search requires sorted input
+    getArray(array, high);
     printf("You entered the following elements: \n");
     printArray(array,high);
     printf("Enter the element to search in the array: \n");
     scanf("%d",&key);
-    int result =BinarySearchRecursive(array, low, high, key);
+    int high1 =high-1;
+    int result =BinarySearchRecursive(array, low, high1, key);
     if(result!=-1)
         printf("The element found at position %d",result);
     else
         printf("The element not found, search unsuccessful \n");
+
     return 0;
 }
